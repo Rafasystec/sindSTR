89e4584f345f6a5e26fff26a31572c20947761ae
Antonio Rocha <rafasystec@yahoo.com.br>
2021-05-17 15:39:31 -0300

    Modifications in some html files and make some entities

--- associados/views.py
+++ associados/views.py
@@ -3,10 +3,10 @@ from django.shortcuts import render
 # Create your views here.
 
 def index(request):
-    return render(request,'index.html')
+    return render(request,'index.html',{'title':'Painel'})
 
 def login(request):
-    return render(request,'login.html')
+    return render(request,'login.html',{'title':'Login'})
 
 def register(request):
-    return render(request,'register.html')
+    return render(request,'register.html',{'title':'Register'})
